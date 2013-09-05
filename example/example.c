75f90a42a12439ae462041033541306b6ff62398
Perry Birch <perrybirch@vizidrix.com>
2013-09-05 03:36:08 -0700

    Adding lrt flag to example

--- example/example.c
+++ example/example.c
@@ -2,7 +2,10 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 void
--- src/benchmark.c
+++ src/benchmark.c
@@ -1,4 +1,4 @@
-#include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
